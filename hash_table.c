41dc524d0ff26e9c4fb7797e45f938162f7c4ddb
Yehor Mishchyriak <89663891+Yehor-Mishchyriak@users.noreply.github.com>
2023-01-16 23:43:32 -0500

    Add files via upload

--- hash_table.c
+++ hash_table.c
@@ -35,7 +35,7 @@ Node* createNode(char* key, char* value, Node* next){
     return newNode;
 }
 
-/* initializes a hash-table */
+/* creates a hash-table */
 HashTable* createHashTable(int capacity){
 
     HashTable* table = calloc(1, sizeof(HashTable));
@@ -51,6 +51,26 @@ HashTable* createHashTable(int capacity){
     return table;
 }
 
+/* deletes a hash-table */
+void clear(HashTable* table){
+
+    // freeing chains
+    for (int bucket = 0; bucket < table -> capacity; bucket++){
+
+            for (Node* current_node = table -> buckets[bucket], *previous_node = current_node;
+                 current_node != NULL;
+                 previous_node = current_node, current_node = current_node -> next)
+
+                free(previous_node);
+    }
+
+    // freeing buckets
+    free(table -> buckets);
+
+    // freeing the table
+    free(table);
+}
+
 /* computes hash of key */
 int hash(char* key, HashTable* table){
 
@@ -61,6 +81,21 @@ int hash(char* key, HashTable* table){
 
     return total % (table -> capacity);
 }
+
+/* tells if the table is empty */
+bool isEmpty(HashTable* table){return table -> current_size == 0;}
+
+/* tells if the table is full */
+bool isFull(HashTable* table){return table -> current_size >= table -> capacity;}
+
+/* returns the current size of the table */
+int currentSize(HashTable* table){return table -> current_size;}
+
+/* returns how full the table is */
+double loadFactor(HashTable* table){return (double) table -> current_size / (double) table -> capacity;}
+
+/* tells if the table is overloaded */
+bool isOverLoaded(HashTable* table){return loadFactor(table) >= 0.7;}
 
 /* prints key : value pairs */
 void keyValuePairs(HashTable* table){
@@ -80,7 +115,7 @@ void newPair(char* key, char* value, HashTable* table){
 
     Node* current_node = table -> buckets[hash(key, table)];
     Node* previous_node = current_node;
-    while (current_node -> next != NULL){
+    while (current_node != NULL){
 
         if (current_node -> key == key){
             current_node -> value = value;
@@ -186,6 +221,46 @@ char** values(HashTable* table){
     return values;
 }
 
+/* returns a copy of a hash-table */
+HashTable* copy(HashTable* table, int capacity){
+
+    HashTable* new_table = createHashTable(capacity);
+
+    for (int bucket = 0; bucket < table -> capacity; bucket++){
+
+            for (Node* current_node = table -> buckets[bucket] -> next;
+                 current_node != NULL;
+                 current_node = current_node -> next)
+
+                newPair(current_node -> key,
+                        current_node -> value,
+                        new_table);
+    }
+
+    return new_table;
+}
+
+/* returns the number of collided keys in the table */
+int numberOfCollisions(HashTable* table){
+
+    if (table -> capacity < 2) return table -> current_size;
+
+    int collisions = 0;
+    char** retrieved_keys = keys(table);
+
+    for (int i = 0, current_hash; i < table -> current_size; i++){
+
+        current_hash = hash(retrieved_keys[i], table);
+        for (int j = 0; j < table -> current_size; j++){
+
+            if (i == j) continue;
+            else if (current_hash == hash(retrieved_keys[j], table)) collisions++;
+        }
+    }
+
+    return collisions / 2;
+}
+
 /* prints an array of keys or values */
 void print_array(char** array, int size){
 
@@ -197,21 +272,19 @@ void print_array(char** array, int size){
 
 int main(void){
 
-    HashTable* table1 = createHashTable(10);
+    HashTable* table1 = createHashTable(50);
+
     newPair("Yehor", "+380663557788", table1);
     newPair("Ihor", "+380674691167", table1);
     newPair("Dmitro", "+380993421872", table1);
+    newPair("Pavel", "+380673272829", table1);
 
-    print_array(keys(table1), table1 -> current_size);
-    print_array(values(table1), table1 -> current_size);
+    HashTable* table2 = copy(table1, 5);
+    newPair("Alexey", "+380993289910", table2);
+
+    printf("table1: load is %f; # of collisions is %d;\ntable2: load is %f; # of collisions is %d;\n",
+            loadFactor(table1), numberOfCollisions(table1), loadFactor(table2), numberOfCollisions(table2));
 
     return 1;
 }
 
-
-/* Additionally, the data structure needs:
- *
- * (1) isEmpty and isFull functions;
- * (2) clear function to delete the dictionary;
- * (3) copy to create a copy of a dictionary;
- */
