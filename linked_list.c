2f2b185e2b8b56cd8f34965704c8f80ccc7974df
Yehor Mishchyriak <89663891+Yehor-Mishchyriak@users.noreply.github.com>
2023-02-02 00:48:48 -0500

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,79 +1,112 @@
-/* LINKED LIST DATA STRUCTURE; SINGLY_LINKED LISTS WITH THE SENTINEL NODE
- * YEHOR MISHCHYRIAK. WESLEYAN UNIVERSITY
- */
+/* Yehor Mishchyriak, Wesleyan University, class of 2026;
+ * Realization of a singly-linked list */
 
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
-
+/* node type */
 typedef struct Node{
 
-    int value;
+    char* value;
     struct Node* next;
 }Node;
 
+/* list type */
+typedef struct{
 
-// methods for the node type
-Node* create(int data){
+    Node* head;
+    int currentSize;
+}List;
 
-    Node* node = calloc(1, sizeof(Node));
-    node -> next = NULL;
-    node -> value = data;
-    return node;
+/* creates a node */
+Node* createNode(char* value){
+
+    Node* NewNode = calloc(1, sizeof(Node));
+    NewNode -> next = NULL;
+    NewNode -> value = value;
+    return NewNode;
 }
 
+/* adds an element to the end of the list */
+void append(List* list, char* value){
 
-void append(Node* head, int data){
-
-    Node* current_node = head;
+    Node* current_node = list -> head;
     while (current_node -> next != NULL)
         current_node = current_node -> next;
 
-    current_node -> next = create(data);
+    current_node -> next = createNode(value);
+    list -> currentSize++;
 }
 
+/* creates a list of the passed elements */
+/* vargs type to be passed is <char*> */
+List* createListOf(int length, ...){
 
-int pop(Node* head){
+    va_list args;
+    if (length < 0){va_end(args); return NULL;}
 
-    Node* current_node = head;
-    if (current_node -> next == NULL) return -1;
+    List* newList = calloc(1, sizeof(List));
+    newList -> head = createNode("dummy");
+    newList -> currentSize = 0;
 
-    Node* previous_node = head;
+    va_start(args, length);
+
+    for (int i = 0; i < length; i++)
+        append(newList, va_arg(args, char*));
+
+    va_end(args);
+
+    return newList;
+}
+
+/* returns the last element of the list */
+/* having deleted it beforehand */
+char* pop(List* list){
+
+    Node* current_node = list -> head;
+    if (current_node -> next == NULL) return "NULL";
+
+    Node* previous_node = list -> head;
     while (current_node -> next != NULL){
         previous_node = current_node;
         current_node = current_node -> next;
     }
 
-    int data_to_pop = current_node -> value;
+    char* data_to_pop = current_node -> value;
     free(current_node);
     previous_node -> next = NULL;
+
+    list -> currentSize--;
     return data_to_pop;
 }
 
+/* inserts an element at the specified location in the list */
+void insert(List* list, char* value, int location){
 
-void insert(Node* head, int data, int location){
+    Node* newNode = createNode(value);
 
-    Node* new_node = create(data);
-
-    Node* current_node = head;
+    Node* current_node = list -> head;
     int current_location = 0;
     while (current_location < location && current_node -> next != NULL){
         current_node = current_node -> next;
         current_location++;
     }
 
-    new_node -> next = current_node -> next;
-    current_node -> next = new_node;
+    newNode -> next = current_node -> next;
+    current_node -> next = newNode;
+    list -> currentSize++;
 }
 
+/* deletes an element from the specified location in the list */
+/* if location > (list -> currentSize) */
+/* the last element is deleted */
+void del(List* list, int location){
 
-void delete(Node* head, int location){
-
-    Node* current_node = head;
+    Node* current_node = list -> head;
     if (current_node -> next == NULL) return;
 
-    Node* previous_node = head;
+    Node* previous_node = list -> head;
     int current_location = -1;
     do {
         previous_node = current_node;
@@ -83,14 +116,17 @@ void delete(Node* head, int location){
 
     previous_node -> next = current_node -> next;
     free(current_node);
+    list -> currentSize--;
 }
 
+/* removes the specified value from the list */
+void remove_value(List* list, char* value_to_remove){
 
-void remove_value(Node* head, int value_to_remove){
+    Node* current_node = list -> head;
+    Node* previous_node = list -> head;
+    while (current_node -> next != NULL &&
+          current_node -> value != value_to_remove){
 
-    Node* current_node = head;
-    Node* previous_node = head;
-    while (current_node -> next != NULL && current_node -> value != value_to_remove){
         previous_node = current_node;
         current_node = current_node -> next;
     }
@@ -98,13 +134,14 @@ void remove_value(Node* head, int value_to_remove){
     if (current_node -> value == value_to_remove){
         previous_node -> next = current_node -> next;
         free(current_node);
+        list -> currentSize--;
     } else return;
 }
 
+/* returns the location of the specified element */
+int find(List* list, char* data_to_find){
 
-int find(Node* head, int data_to_find){
-
-    Node* current_node = head;
+    Node* current_node = list -> head;
     int index = -1;
     while (current_node != NULL){
 
@@ -118,24 +155,13 @@ int find(Node* head, int data_to_find){
     return -1;
 }
 
+/* returns the current length of the list */
+int len(List* list){return list -> currentSize;}
 
-int len(Node* head){
+/* returns the number of the instances of the specified element */
+int count(List* list, char* instance){
 
-    Node* current_node = head;
-    int length = 0;
-
-    while (current_node -> next != NULL){
-        current_node = current_node -> next;
-        length++;
-    }
-
-    return length;
-}
-
-
-int count(Node* head, int instance){
-
-    Node* current_node = head;
+    Node* current_node = list -> head;
     int number_of_instances = 0;
 
     while (current_node -> next != NULL){
@@ -147,20 +173,18 @@ int count(Node* head, int instance){
     return number_of_instances;
 }
 
+/* prints the list */
+void printList(List* list){
 
-void print_list(Node* head){
+    Node* current_node = list -> head;
 
-    Node* current_node = head;
     while(current_node -> next != NULL){
         current_node = current_node -> next;
-        printf("%d ", current_node -> value);
-    }
-    printf("\n");
+        printf("%s ", current_node -> value);
+    }printf("\n");
 }
-
 
 int main(void){
 
     return 1;
 }
-
